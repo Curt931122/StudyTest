9c856d5cb8c47f9a9e5c1d541ada1a188daaa701
chenguangjin <chenguangjin@hywx-pc14.hy.local>
2017-11-29 16:01:55 +0800

    test

--- guangjinTest/src/main/java/com/study/thread/RunnableDemo.java
+++ guangjinTest/src/main/java/com/study/thread/RunnableDemo.java
@@ -11,14 +11,16 @@ public class RunnableDemo implements Runnable {
    
    public void run() {
       System.out.println("Running " +  threadName );
-      try {
-         for(int i = 4; i > 0; i--) {
-            System.out.println("Thread: " + threadName + ", " + i);
-            // 让线程睡眠一会
-            Thread.sleep(50);
-         }
-      }catch (InterruptedException e) {
-         System.out.println("Thread " +  threadName + " interrupted.");
+      {
+	      try {
+	         for(int i = 4; i > 0; i--) {
+	            System.out.println("Thread: " + threadName + ", " + i);
+	            // 让线程睡眠一会
+	            Thread.sleep(100);
+	         }
+	      }catch (InterruptedException e) {
+	         System.out.println("Thread " +  threadName + " interrupted.");
+	      }
       }
       System.out.println("Thread " +  threadName + " exiting.");
    }
